dc85d9f7640a6a59c3c4e81640cb6e5bc52c1aae
Michael Ramstein <michael@ramste.in>
2022-04-27 16:20:34 +0200

    0.10.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule OpenXchangeClient.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/mrmstn/open-xchange-client"
-  @version "0.10.1"
+  @version "0.10.2"
 
   def project do
     [
